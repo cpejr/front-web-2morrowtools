e3c02fb4249340da714091a726d812d1ca6cc619
Ludmila Carvalho <ludmilamsaid@gmail.com>
2023-11-16 18:16:50 -0300

    feat: image bank test

--- src/pages/NewTool/NewTool.jsx
+++ src/pages/NewTool/NewTool.jsx
@@ -1,5 +1,51 @@
-import { Container } from "./Styles";
+import { Container, BlueButton } from "./Styles";
+import { Upload, Button, message } from "antd";
+import { UploadOutlined, EditOutlined, DeleteOutlined, SendOutlined } from "@ant-design/icons";
+import React, { useState } from "react";
+import techImage from "../../assets/tech.jpeg";
 
 export default function NewTool() {
-  return <Container>AddIA</Container>;
+  const [fileList, setFileList] = useState([]);
+
+  const onChange = (info) => {
+    setFileList(info.fileList);
+  };
+
+  const onRemove = (file) => {
+    setFileList((prev) => prev.filter((f) => f.uid !== file.uid));
+  };
+
+  const updateImage = ({ onSuccess }) => {
+    setTimeout(() => {
+      onSuccess("ok");
+      message.success("Imagem enviada com sucesso!");
+    }, 1000);
+  };
+
+  return (
+    <Container>
+      <img src={techImage} />
+      <Upload
+        fileList={fileList}
+        customRequest={updateImage}
+        onChange={onChange}
+        onRemove={onRemove}
+        showUploadList={{ showPreviewIcon: false, showRemoveIcon: true }}
+      >
+        <Button icon={<UploadOutlined />}>Upload</Button>
+      </Upload>
+      <BlueButton>
+        <EditOutlined />
+        Alterar
+      </BlueButton>
+      <BlueButton>
+        <DeleteOutlined />
+        Deletar
+      </BlueButton>
+      <BlueButton>
+        <SendOutlined />
+        Salvar Imagem
+      </BlueButton>
+    </Container>
+  );
 }
--- src/pages/NewTool/Styles.js
+++ src/pages/NewTool/Styles.js
@@ -1,6 +1,49 @@
+import { Button } from "antd";
+
 import styled from "styled-components";
+import { colors } from "../../styles/styleVariables";
+
+const firstBreakPoint = "1370px";
+const secondBreakPoint = "1024px";
+const thirdBreakPoint = "800px";
+const mobileBreakPoint = "700px";
 
 export const Container = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 150px;
+  margin-bottom: 100px;
   flex-direction: column;
+
+  width: 60%;
+  // ou qualquer valor desejado
+  margin-left: 20%;
+`;
+export const BlueButton = styled(Button)`
+  width: 150px;
+  height: 50px;
+  background-color: ${colors.blue.accent};
+  border: none;
+  margin-top: 1rem;
+  @media (max-width: ${firstBreakPoint}) {
+    width: 128px;
+    height: 40px;
+  }
+  @media (max-width: ${secondBreakPoint}) {
+    width: 96px;
+    height: 30px;
+  }
+
+  @media (max-width: ${thirdBreakPoint}) {
+    width: 86.4px;
+    height: 27px;
+  }
+  @media (max-width: ${mobileBreakPoint}) {
+    width: 128px;
+    height: 40px;
+  }
+  &:hover {
+    background-color: ${colors.blue.hover};
+  }
 `;
