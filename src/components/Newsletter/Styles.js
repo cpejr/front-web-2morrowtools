714e8ed6e826f1585bb6ebfa75fc08b3c692eef8
gabrielhermont <138725279+gabrielhermont@users.noreply.github.com>
2024-01-21 18:21:06 -0300

    fix; newsletter properties

--- src/components/Newsletter/Styles.js
+++ src/components/Newsletter/Styles.js
@@ -9,31 +9,36 @@ export const Container = styled.div`
   gap: 20px;
   padding: 20px;
   max-width: 80vw;
-  box-shadow: -2px -1px 5px white;
+  box-shadow: -2px -1px 5px 0.5px white, 2px 2px 2px 1px grey;
   flex-wrap: wrap;
+  height: 150px;
 `;
-export const NameInput = styled(Input)`
-  border-radius: 3px;
+export const Input1 = styled(Input)`
   width: 100%;
+  font-weight: 300;
+  font-size: large;
+  border: 2px solid #240079;
 `;
 
-export const SendButton = styled(Button)`
-  color: #a883ff;
-  border-color: #a883ff;
-  width: 50%;
+export const Button1 = styled(Button)`
+  color: #240079;
+  font-weight: bold;
+  border-color: #240079;
+  width: 40%;
   align-self: center;
 `;
 
-export const Text1 = styled.div`
+export const Title = styled.div`
   font-size: larger;
   font-weight: bold;
 `;
-export const Text2 = styled.div``;
+export const Description = styled.div``;
 
 export const Texts = styled.div`
   display: flex;
   flex-direction: column;
   width: 55%;
+  justify-content: center;
   @media (max-width: ${breakpoints.newsletter}) {
     width: 100%;
   }
@@ -43,6 +48,7 @@ export const Inputs = styled.div`
   flex-direction: column;
   gap: 10px;
   width: 40%;
+  justify-content: center;
   @media (max-width: ${breakpoints.newsletter}) {
     width: 100%;
     align-self: center;
