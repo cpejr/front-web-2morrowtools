7c72b569940e5a9f79bfb1b39d3cbb7e9958e940
SamuelFVG <samuelfelipeif@gmail.com>
2024-01-23 10:17:43 -0300

    fix: add auth token to request

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,22 @@
 import axios from "axios";
+import useAuthStore from "../stores/auth";
 
 const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}`;
 const api = axios.create({
   baseURL: BASE_URL,
 });
+
+api.interceptors.request.use(
+  (req) => {
+    const { token } = useAuthStore.getState();
+
+    if (!req.headers.Authorization && token) {
+      req.headers.Authorization = `Bearer ${token}`;
+    }
+    return req;
+  },
+
+  (error) => Promise.reject(error)
+);
 
 export default api;
