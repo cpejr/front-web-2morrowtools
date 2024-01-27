323825d68bb47a2799627c309a87eb57dddd7884
Luiz Verteiro <luizverteiro1819@gmail.com>
2024-01-27 10:03:50 -0300

    feat: add logic for exporting an array

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -1,19 +1,42 @@
 import { Container, Title, Description, Texts, Inputs, Break, Input, Button } from "./Styles";
-import { usePostNewsletter } from "../../services/ManagerService";
-import { useState } from "react";
+import { usePostNewsletter, useGetNewsletter } from "../../services/ManagerService";
+import { useState, useEffect } from "react";
+import { saveAs } from "file-saver";
 
 export default function Newsletter() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, SetMessage] = useState("");
+  const [newsletters, setNewsletters] = useState([]);
 
-  async function PostNewsletter() {
+  useEffect(() => {
+    async function fetchData() {
+      const result = await useGetNewsletter();
+      setNewsletters(result.Newsletters);
+    }
+
+    fetchData();
+  }, []); 
+
+  const PostNewsletter = async () => {
     await usePostNewsletter({
       name: name,
       email: email,
       message: message,
     });
-  }
+  };
+
+  const exportData = () => {
+    const csvContent =
+      "Nome,Email,Mensagem\n" +
+      newsletters
+        .map((newsletter) => [newsletter.name, newsletter.email, newsletter.message].join(","))
+        .join("\n");
+
+    const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+    saveAs(blob, "newsletter_data.csv");
+  };
+
   return (
     <Container>
       <Texts>
@@ -31,6 +54,9 @@ export default function Newsletter() {
         <Input onChange={(e) => SetMessage(e.target.value)} placeholder='Mensagem:'></Input>
         <Button onClick={PostNewsletter} type='primary'>
           ENVIAR
+        </Button>
+        <Button onClick={exportData} type='secondary'>
+          EXPORTAR
         </Button>
       </Inputs>
     </Container>
