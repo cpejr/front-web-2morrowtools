e2e34730441b84c034c5fdd687c2b8239484eae9
Rian-Rero <rian@dharma.com.br>
2024-01-28 20:28:17 -0300

    fix: remove useless icon

--- src/components/Tool/Tool.jsx
+++ src/components/Tool/Tool.jsx
@@ -33,7 +33,6 @@ import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
 import { FaRegBookmark } from "react-icons/fa";
 import { FaStarHalfStroke } from "react-icons/fa6";
-import { IoShareSocial } from "react-icons/io5";
 import { RiStarSLine, RiStarSFill } from "react-icons/ri";
 import useAuthStore from "../../stores/auth";
 import {
