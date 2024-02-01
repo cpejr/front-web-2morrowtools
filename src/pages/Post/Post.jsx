335e0c2198fc1d4861b83b339becf9985a99ec67
Mateus Bretz <mateusbretz@cpejr.com.br>
2024-02-01 16:31:47 -0300

    feat: creating the post specific page

---
+++ src/pages/Post/Post.jsx
@@ -0,0 +1,19 @@
+import { Container } from "./Styles";
+
+export default function Post() {
+  const posts = [
+    {
+      title: "Novidades da ferramenta",
+      categories: "neibciuebciubeuibcui",
+      shortDescription: "tcvuycv cebucebc",
+      imageUrl: "https://picsum.photos/id/237/536/354",
+      longDescription: "Sed ut perspiciatis unde omnis iste natus error sit",
+    },
+  ];
+
+  return (
+    <Container>
+      <div>Posts</div>
+    </Container>
+  );
+}
---
+++ src/pages/Post/Styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 85px;
+  margin-bottom: 100px;
+  gap: 2rem;
+
+  div {
+    font-size: 3rem;
+    margin-bottom: 1rem;
+    color: #240079;
+  }
+`;
