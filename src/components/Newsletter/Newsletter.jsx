f17c05a63a0b6346bad1083236056728d196389a
Lucas Velloso <lucasabreuvelloso@gmail.com>
2024-02-05 14:37:00 -0300

    fix: cleaning after submit

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -1,79 +1,3 @@
-// import { Container, Title, Description, Texts, Inputs, Break, Input, Button, Form } from "./Styles";
-// import { usePostNewsletter, useGetNewsletter } from "../../services/ManagerService";
-// import { useState, useEffect } from "react";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import { toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import { useForm } from "react-hook-form";
-
-// export default function Newsletter() {
-//   const [name, setName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [message, SetMessage] = useState("");
-//   const [newsletters, setNewsletters] = useState([]);
-
-//   const { handleSubmit, register } = useForm({});
-//   const onSubmit = async () => {
-//     // await PostNewsletter();
-//     await PostNewsletter();
-//     try {
-//       await usePostNewsletter({
-//         name: name,
-//         email: email,
-//         message: message,
-//       });
-//       toast.success("Email cadastrado!");
-//     } catch (error) {
-//       toast.error("Cadastro invalido!");
-//     }
-//   };
-
-//   useEffect(() => {
-//     async function fetchData() {
-//       const result = await useGetNewsletter();
-//       setNewsletters(result.Newsletters);
-//     }
-
-//     fetchData();
-//   }, []);
-//   return (
-//     <Container>
-//       <Form onSubmit={handleSubmit(onSubmit)}>
-//         <Texts>
-//           <Title>Descubra novas ferramentas de tecnologia toda semana! </Title>
-//           <Description>
-//             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-//             incididunt ut labore et dolore magna aliqua
-//           </Description>
-//         </Texts>
-//         <Inputs>
-//           <Break>
-//             <Input
-//               name='name'
-//               register={"name"}
-//               // onChange={(e) => setName(e.target.value)}
-//               placeholder='Nome:'
-//             ></Input>
-//             <Input
-//               name='email'
-//               register={"email"}
-//               // onChange={(e) => setEmail(e.target.value)}
-//               placeholder='Email:'
-//             ></Input>
-//           </Break>
-//           <Input
-//             name='message'
-//             register={"message"}
-//             // onChange={(e) => SetMessage(e.target.value)}
-//             placeholder='Mensagem:'
-//           ></Input>
-//           <Button type='submit'>ENVIAR</Button>
-//         </Inputs>
-//       </Form>
-//     </Container>
-//   );
-// }
-
 import { Container, Title, Description, Texts, Inputs, Break, Input, Button } from "./Styles";
 import { usePostNewsletter, useGetNewsletter } from "../../services/ManagerService";
 import { useState, useEffect } from "react";
@@ -109,16 +33,12 @@ export default function Newsletter() {
     }
   };
 
-  // const exportData = () => {
-  //   const csvContent =
-  //     "Nome,Email,Mensagem\n" +
-  //     newsletters
-  //       .map((newsletter) => [newsletter.name, newsletter.email, newsletter.message].join(","))
-  //       .join("\n");
-
-  //   const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
-  //   saveAs(blob, "newsletter_data.csv");
-  // };
+  const handleSubmit = () => {
+    PostNewsletter();
+    setName("");
+    setEmail("");
+    SetMessage("");
+  };
 
   return (
     <Container>
@@ -131,11 +51,19 @@ export default function Newsletter() {
       </Texts>
       <Inputs>
         <Break>
-          <Input onChange={(e) => setName(e.target.value)} placeholder='Nome:'></Input>
-          <Input onChange={(e) => setEmail(e.target.value)} placeholder='Email:'></Input>
+          <Input onChange={(e) => setName(e.target.value)} value={name} placeholder='Nome:'></Input>
+          <Input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            placeholder='Email:'
+          ></Input>
         </Break>
-        <Input onChange={(e) => SetMessage(e.target.value)} placeholder='Mensagem:'></Input>
-        <Button onClick={PostNewsletter} type='submit'>
+        <Input
+          onChange={(e) => SetMessage(e.target.value)}
+          value={message}
+          placeholder='Mensagem:'
+        ></Input>
+        <Button onClick={handleSubmit} type='submit'>
           ENVIAR
         </Button>
         {/* <Button onClick={exportData} type='secondary'>
